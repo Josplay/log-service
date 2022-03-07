2e1f1adea9ab6ebae418034659134a2d167e38fa
Mundane Lizard <mundanelizard@gmail.com>
2022-03-07 11:48:38 +0100

    cleaned up server

--- index.js
+++ index.js
@@ -5,21 +5,17 @@ const http = require('http')
 const cors = require('cors')
 const { connectDb,  PORT, logger } = require('./config')
 
-async function main() {
-  const app = express()
+const app = express()
 
-  app.use(cors())
-  app.use(express.json())
-  app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-  const db = await connectDb()
+const db = connectDb()
 
-  app.use('/v1/', generateV1(db))
+app.use('/v1/', generateV1(db))
 
-  const server = http.createServer(app)
+const server = http.createServer(app)
 
-  server.listen(PORT, () => console.log('Listening at port ', PORT))
-  server.on('error', logger.error)
-}
-
-main()
+server.listen(PORT, () => console.log('Listening at port ', PORT))
+server.on('error', logger.error)
